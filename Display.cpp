954aa90da790fe6c00321b9cca170e7963977c1f
Christian Brantner <cabrantner@csu.fullerton.edu>
2023-02-02 16:19:33 -0800

    display cpp

---
+++ Display.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Display.h"
+using namespace std;
+
+void DisplayNames(string file) {
+	ifstream infile;
+	infile.open("file");
+	string line;
+	
+	if (infile.is_open()) {
+		while (infile) {
+			getline(infile, line);
+			cout << line << endl;
+		}
+	}
+	else {
+		cout << "Could not open file.\n";
+	}
+	return 0;
+}
+
